83d48b981695915c265efcc2f7dc86e714dce84f
seanhardison1 <seanhardison@gmail.com>
2021-02-16 07:49:36 -0500

    fixes issue introduced by multiple questions on same day

--- R/participation.R
+++ R/participation.R
@@ -14,6 +14,7 @@ template <- read.csv(here::here("data/gradebook-21Sp_Atmosphere_and_Weather-ALL.
   unite(col = "student", c(First, Last), sep = " ")
 
 # read PE data
+df <- tibble()
 for (i in 1:length(list.files(pe_dir))){
   
   all_lines <- suppressWarnings(readLines(file.path(pe_dir,
@@ -28,29 +29,21 @@ for (i in 1:length(list.files(pe_dir))){
     dplyr::rename(student = Registered.participant) %>% 
     dplyr::select(Created.At, student) %>% 
     mutate(Participation = 1,
-           Created.At = round_date(ymd_hms(Created.At), unit = "10 minutes"))
+           Created.At = round_date(ymd_hms(Created.At), unit = "10 minutes")) %>% 
+    right_join(.,template) %>% 
+    mutate(Participation = replace_na(Participation, 0),
+           Created.At = mean(unique(Created.At), na.rm = T)) 
   
-  if (i == 1){
-    joined_partic <- 
-      template %>% 
-      left_join(.,init) %>% 
-      mutate(Participation = replace_na(Participation, 0)) %>% 
-      dplyr::select(student, Participation)
-  } else {
-    joined_partic %<>% 
-      left_join(.,init) %>% 
-      mutate(Participation = replace_na(Participation, 0)) %>% 
-      dplyr::select(-Created.At)
-  }
-  
-  names(joined_partic)[ncol(joined_partic)] <- paste0("Participation ",
-                                    mean(unique(init$Created.At)))
-  print(names(joined_partic)[ncol(joined_partic)])
+  assign('df', rbind(df, init))
 }
 
 # output grades for import into collab
-grades <- left_join(template, joined_partic) %>% 
-  dplyr::rename(`Student ID` = id,
-                "# Student Name" = student)
+grades <- 
+  df %>% 
+  group_by(lect_id = paste0("Participation ",as.Date(Created.At)), 
+           `Student ID` = id,
+           "# Student Name" = student) %>% 
+  dplyr::summarise(Participation = sum(Participation)) %>% 
+  pivot_wider(names_from = lect_id, values_from = Participation)
 
 write.csv(grades, file = file.path(out_dir, "grades.csv"), row.names = F)
